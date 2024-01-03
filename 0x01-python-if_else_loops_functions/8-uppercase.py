d807da477f6cb074ab7490a6a7ee1c6d914feb44
Julius Charles <cjmatheka@gmail.com>
2024-01-03 19:42:16 +0300

    Add task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    result = ""
+    if str is not None:
+        for char in str:
+            if 'a' <= char <= 'z':
+                upper = chr(ord(char) - ord('a') + ord('A'))
+                result += upper
+            else:
+                result += char
+    return result
