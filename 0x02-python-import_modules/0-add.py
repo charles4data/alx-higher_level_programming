2e56f605fcdb204e29670ee11170c2538b1a0462
Julius Charles <cjmatheka@gmail.com>
2024-01-05 04:03:51 +0300

    Update Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-# import modules
-from add_0 import add
+if __name__ == "__main__":
+    # import modules
+    from add_0 import add
 
-# Assign values
-a = 1
-b = 2
+    # Assign values
+    a = 1
+    b = 2
 
-# Print function
-print("{} + {} = {}".format(a, b, add(a, b)))
+    # Print function
+    print("{} + {} = {}".format(a, b, add(a, b)))
