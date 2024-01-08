e15e634ff46d1c3ac70113ceed740d5bb5aabaea
Julius Charles <cjmatheka@gmail.com>
2024-01-08 23:09:48 +0300

    Correct Task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 
+# Function for replacing an element in a list
 def replace_in_list(my_list, idx, element):
+
+    # if the index is negative or out of range
     if idx < 0 and idx >= len(my_list):
         return my_list
+
+    # If the index is positive and in range
     else:
         my_list[idx] = element
         return my_list
