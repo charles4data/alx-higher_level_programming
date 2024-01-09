8b9e30bb2d9fb52d845fe6b0812c9ad9c36e88f3
Julius Charles <cjmatheka@gmail.com>
2024-01-10 01:48:05 +0300

    Correct Task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,8 @@
 
 def element_at(my_list, idx):
     size = len(my_list)
-    if idx < 0 or idx >= size:
-        print("None")
+    if 0 > idx or idx >= size:
+        return None
     else:
         for i in my_list:
             return my_list[idx]
