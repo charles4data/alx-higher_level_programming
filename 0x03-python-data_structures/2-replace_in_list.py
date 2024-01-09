ccb9741572f5d624885dfb243caf218dc2742d9d
Julius Charles <cjmatheka@gmail.com>
2024-01-10 01:44:45 +0300

    Correct Task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,7 +4,7 @@
 def replace_in_list(my_list, idx, element):
 
     # if the index is negative or out of range
-    if idx < 0 and idx >= len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
 
     # If the index is positive and in range
