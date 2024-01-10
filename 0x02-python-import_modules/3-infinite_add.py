ea8c2b4f1ac2ac07b1ff8db9f97848eb6a67e5c9
Julius Charles <cjmatheka@gmail.com>
2024-01-10 03:01:49 +0300

    add Task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import sys
+
+def add_arguments(arguments):
+    return sum(int(arg) for arg in arguments)
+
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+    result = add_arguments(arguments)
+    print(result)
