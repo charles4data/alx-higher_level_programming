76f0a502b9c3f831e7044ba124db064f4bfd89c5
Julius Charles <cjmatheka@gmail.com>
2024-01-12 00:03:09 +0300

    remove 0x17. It was added into the wrong folder

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     def calculate_values():
         # importing modules
         from calculator_1 import sub, add, mul, div
-
         # assigning variables
         a = 10
         b = 5
-
         # printing results
         print("{} + {} = {}".format(a, b, add(a, b)))
         print("{} - {} = {}".format(a, b, sub(a, b)))
