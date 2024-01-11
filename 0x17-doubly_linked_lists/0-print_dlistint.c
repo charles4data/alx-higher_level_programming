1398ba7213ac189465f77d4fff4bee3f8cc8e40f
Julius Charles <cjmatheka@gmail.com>
2024-01-11 23:51:12 +0300

    Correct Task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,17 +10,17 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-    const dlistint_t *current;
-    size_t nodeCount;
+	const dlistint_t *current;
+	size_t nodeCount;
 
-    current = h;
-    nodeCount = 0;
+	current = h;
+	nodeCount = 0;
 
-    while (current != NULL)
-    {
-        printf("%d\n", current -> n);
-        current = current -> next;
-        nodeCount ++;
-    }
-    return (nodeCount);
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		nodeCount++;
+	}
+	return (nodeCount);
 }
