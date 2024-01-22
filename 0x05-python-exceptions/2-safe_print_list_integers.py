e364474b564d8b35a7a7a6bf42f4d5368bc2b104
Julius Charles <cjmatheka@gmail.com>
2024-01-22 21:25:06 +0300

    Add Task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    length = 0
+    try:
+        for i in range(x):
+            try:
+                print("{:d}".format(my_list[i]), end="")
+                length += 1
+
+            except (ValueError, TypeError):
+                pass
+
+    except IndexError:
+        print("List out of range")
+
+    print()
+    return length
