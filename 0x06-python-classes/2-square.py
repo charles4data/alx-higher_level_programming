36641a361611b1f88011deeff8089adef62b18e7
Julius Charles <cjmatheka@gmail.com>
2024-01-24 17:03:54 +0300

    Update Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,18 +9,9 @@ class Square:
     def __init__(self, size=0):
         """ The size attribute defines the size of a square
         Arguments: int(size)"""
+        assert size == int, "Size must be an integer"
+        assert size >= 0, "size must be >= 0"
+
+        # instantiate size
         self._size = size
 
-    @property
-    def size(self):
-        return self._size
-
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("Size must be an integer")
-
-        if not value >= 0:
-            raise ValueError("Size must be greater than or equal to 0")
-
-        self._size = value
