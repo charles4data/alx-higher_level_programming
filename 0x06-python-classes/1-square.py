043ae2eab5eec10fbd27cc023435770225d00b9a
Julius Charles <cjmatheka@gmail.com>
2024-01-24 14:00:26 +0300

    Add Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,7 +4,9 @@
 
 
 class Square:
-    """This class defines a square"""
+    """This class defines a square
+    Attributes: int(size) - means size of a square"""
     def __init__(self, size):
-        """ The size attribute defines the size of a square"""
+        """ The size attribute defines the size of a square
+        Arguments: int(size)"""
         self._size = size
