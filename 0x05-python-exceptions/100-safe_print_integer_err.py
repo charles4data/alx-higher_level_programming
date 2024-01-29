0d9676356ef3a82f9aff915cc669fb1976a61424
cjmatheka <cjmatheka@gmail.com>
2024-01-29 22:44:43 +0300

    Add initial files for tasks 8 and 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,6 +5,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except TypeError as e:
+    except (TypeError, ValueError) as e:
         print(f"Exception: {e}")
         return False
