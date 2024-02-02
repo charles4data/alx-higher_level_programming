f02e61d155f4088696fce2d924db09ca7cdc62c6
cjmatheka <cjmatheka@gmail.com>
2024-02-02 18:44:24 +0300

    Update Task 5

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+"""
+A function that prints 2 new lines after each of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    # Checking if text is a string
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    # Define characters that trigger new lines
+    new_line_chars = ['.', '?', ':']
+
+    for char in text:
+        if char not in new_line_chars:
+            print(char, end='')
+        else:
+            print(char)
+            print()
+
+    if text[-1] not in new_line_chars:
+        print()
