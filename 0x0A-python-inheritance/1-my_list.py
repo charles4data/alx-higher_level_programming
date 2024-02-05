0856cb5958cde07a73431d8d0dec33c33b55b984
cjmatheka <cjmatheka@gmail.com>
2024-02-05 20:01:16 +0300

    Update Task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,3 +4,7 @@ class MyList(list):
     def print_sorted(self):
         sorted_list = sorted(self)
         print(sorted_list)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
