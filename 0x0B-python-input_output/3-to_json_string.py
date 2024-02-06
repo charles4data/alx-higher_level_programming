e623f95b465b83f8368cfd8bd15a9ff048374be9
cjmatheka <cjmatheka@gmail.com>
2024-02-07 02:12:01 +0300

    Add Task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+"""
+serializes objects into json notation
+"""
+
+
+def to_json_string(my_obj):
+    """
+    returns the json representation of an object
+    my_obj: object tp jsonify
+    return: json representation
+    """
+    import json
+    return json.dumps(my_obj)
