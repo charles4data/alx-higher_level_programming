9aaccbefd75811d35104c3601cdfe9df98271156
cjmatheka <cjmatheka@gmail.com>
2024-02-07 02:04:16 +0300

    Update task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,6 +6,12 @@ An append write function
 
 
 def append_write(filename="", text=""):
+    """
+    Appends text and counts characters added
+    filename: name of the file to write into
+    text: The text to write into file
+    return: num of characters written
+    """
     with open(filename, mode="a", encoding="utf-8") as file:
         file.write(text)
-    return len(text)
+        return len(text)
