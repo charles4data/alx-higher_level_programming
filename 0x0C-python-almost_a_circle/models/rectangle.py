88ece2569af384609b51feced2b03e51d6397a5d
cjmatheka <cjmatheka@gmail.com>
2024-02-16 07:16:47 +0300

    Add Task 3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 
-from models.base import Base
-
 """
 A subclass of Base class
 """
+
+from models.base import Base
 
 
 class Rectangle(Base):
     """
     Inherits from base class and defines a rectangle
+    Adds attributes width, height, x and y
+    Defines getters and setters for each attribute, except id
     """
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
@@ -18,38 +20,113 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
 
-        # Getter and setter methods for width
-        @property
-        def width(self):
-            return self.__width
+    @property
+    def width(self):
+        """
+        Getter method for retrieving the value of width attribute
+        self: Instance object
+        return: width value of instance object
+        """
+        return self.__width
 
-        @width.setter
-        def width(self, value):
+    @width.setter
+    def width(self, value):
+        """
+        Setter method for width, integrating validation methods
+        self: Instance object
+        value: new value for width, to be updated
+        raises:
+            TypeError: if value is not an integer
+            ValueError: if value is zero or negative
+        return: Nothing, only updates the width value
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be greater than 0")
+        else:
             self.__width = value
 
-        # Getter and setter methods for height
-        @property
-        def height(self):
-            return self.__height
+    # Getter and setter methods for height
+    @property
+    def height(self):
+        """
+        Getter method for retrieving the value of the height attribute
+        self: Instance object
+        return: height value of instance object
+        """
+        return self.__height
 
-        @height.setter
-        def height(self, value):
+    @height.setter
+    def height(self, value):
+        """
+        Setter method for height, integrating validation methods
+        self: Instance object
+        value: new value for height, to be updated
+        raises:
+            TypeError: if value is not an integer
+            ValueError: if value is zero or negative
+        return: Nothing, only updates the height value
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
             self.__height = value
 
-        # Getter and setter methods for x
-        @property
-        def x(self):
-            return self.__x
+    # Getter and setter methods for x
+    @property
+    def x(self):
+        """
+        Getter method for retrieving the value of the x
+        self: Instance object
+        return: x value of instance object
+        """
+        return self.__x
 
-        @x.setter
-        def x(self, value):
+    @x.setter
+    def x(self, value):
+        """
+        Setter method for x, integrating validation methods
+        self: Instance object
+        value: new value for x, to be updated
+        raises:
+            TypeError: if value is not an integer
+            ValueError: if value is zero or negative
+        return: Nothing, only updates the x value
+        """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        else:
             self.__x = value
 
-        # Getter and setter methods for y
-        @property
-        def y(self):
-            return self.__y
+    # Getter and setter methods for y
+    @property
+    def y(self):
+        """
+        getter method for retrieving value of y
+        self: Instance object
+        return: returns value of y
+        """
+        return self.__y
 
-        @y.setter
-        def y(self, value):
+    @y.setter
+    def y(self, value):
+        """
+        Setter method for y, integrating validation methods
+        self: Instance object
+        value: new value for x, to be updated
+        raises:
+            TypeError: if value is not an integer
+            ValueError: if value is zero or negative
+        return: Nothing, only updates the y value
+        """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        else:
             self.__y = value
