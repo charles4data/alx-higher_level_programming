f2ab5ba61f53ef98c3d877d6e8f0fafd9d191a7a
cjmatheka <cjmatheka@gmail.com>
2024-02-16 06:38:45 +0300

    Add Task 2

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+from base import Base
+
+"""
+A subclass of Base class
+"""
+
+
+class Rectangle(Base):
+    """
+    Inherits from base class and defines a rectangle
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+        # Getter and setter methods for width
+        @property
+        def width(self):
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            self.__width = value
+
+        # Getter and setter methods for height
+        @property
+        def height(self):
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            self.__height = value
+
+        # Getter and setter methods for x
+        @property
+        def x(self):
+            return self.__x
+
+        @x.setter
+        def x(self, value):
+            self.__x = value
+
+        # Getter and setter methods for y
+        @property
+        def y(self):
+            return self.__y
+
+        @y.setter
+        def y(self, value):
+            self.__y = value
