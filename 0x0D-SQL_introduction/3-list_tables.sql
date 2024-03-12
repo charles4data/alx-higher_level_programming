6f3bb6c647fd16998ebe0bac9b8809acd022cf9c
cjmatheka <cjmatheka@gmail.com>
2024-03-13 02:28:20 +0300

    Update task 3

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,17 +1,3 @@
--- Capture Database name
-SET @db_name = (
-    SELECT SUBSTRING_INDEX(@@version_info, '-', -2)
-);
-
--- Check if the database name is empty
-IF @db_name IS NULL OR @db_name = ''
-BEGIN
-    RAISERROR('Please provide the database name.', 16, 1);
-    RETURN;
-END;
-
--- Switch to the provided database
-USE @db_name;
-
 -- List the tables in the database
+
 SHOW TABLES;
