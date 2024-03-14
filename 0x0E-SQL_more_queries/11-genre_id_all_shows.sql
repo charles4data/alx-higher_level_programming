d843895812c052d189291e2fd63c9bfbabcc9ccc
cjmatheka <cjmatheka@gmail.com>
2024-03-14 23:18:04 +0300

    Update Task 11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- Lists all shows
 
-SELECT tv_shows.title, g.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
   FROM tv_shows
        LEFT JOIN tv_show_genres
        ON tv_shows.id = tv_show_genres.show_id
