962efa31b244a711fba27bd28856f385bc7bc723
cjmatheka <cjmatheka@gmail.com>
2024-03-14 23:28:19 +0300

    Update Task 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,8 +3,6 @@
 SELECT tv_shows.title, tv_shows_genres.genre_id
     FROM tv_shows
     INNER JOIN tv_shows_genres
-        ON tv_shows.genre_id = tv_shows_genres.genre_id
-    GROUP SET (
-        tv_shows.title
-        tv_shows_genres.genre_id
-    )
+        ON tv_shows.id = tv_shows_genres.show_id
+    WHERE tv_shows_genres IS NULL
+    ORDER BY tv_shows.title, tv_shows_genres.genre_id;
