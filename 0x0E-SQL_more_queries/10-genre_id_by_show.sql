328df1ed36e6b5a13b7f5b96ddb1d8488aa3f18d
cjmatheka <cjmatheka@gmail.com>
2024-03-14 04:14:07 +0300

    Correct Task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,7 @@
 -- Lists all shows
 
-SELECT tv_shows.title, v_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
   FROM tv_shows
         INNER JOIN tv_show_genres
-	ON tv_shows.id = v_show_genres.show_id
- ORDER BY tv_shows.title, v_show_genres.genre_id;
+	ON tv_shows.id = tv_show_genres.show_id
+ ORDER BY tv_shows.title, tv_show_genres.genre_id;
