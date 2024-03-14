89acc61e023b338e024a2ee1cee0b23ba807847e
cjmatheka <cjmatheka@gmail.com>
2024-03-14 23:51:42 +0300

    Correct Task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -7,5 +7,5 @@ SELECT tv_genres.name
 
        INNER JOIN tv_shows
        ON tv_shows.id = tv_show_genres.show_id
-       WHERE tv_shows.title = Dexter
+       WHERE tv_shows.title = "Dexter"
  ORDER BY tv_genres.name;
