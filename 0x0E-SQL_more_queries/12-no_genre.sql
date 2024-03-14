7bf6038b6ed9be13f2c03b1ef06fa855d638e339
cjmatheka <cjmatheka@gmail.com>
2024-03-14 23:31:46 +0300

    Update Task 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -4,5 +4,5 @@ SELECT tv_shows.title, tv_shows_genres.genre_id
     FROM tv_shows
     LEFT JOIN tv_shows_genres
         ON tv_shows.id = tv_shows_genres.show_id
-    WHERE tv_shows_genres.genre_id IS NULL
+        WHERE tv_shows_genres.genre_id IS NULL
     ORDER BY tv_shows.title, tv_shows_genres.genre_id;
