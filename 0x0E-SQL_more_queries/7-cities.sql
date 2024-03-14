73557abaed0e9af3183b0ab33129ca58cb438445
cjmatheka <cjmatheka@gmail.com>
2024-03-14 03:40:41 +0300

    Add Task 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- creates a database and cities table
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    state_id INT FOREIGN KEY(states.id) NOT NULL,
+    name VARCHAR(256)
+);
