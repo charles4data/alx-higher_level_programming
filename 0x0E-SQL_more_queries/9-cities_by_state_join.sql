f2d7debac1872b8c5673ea51aed22fbafd9a40fc
cjmatheka <cjmatheka@gmail.com>
2024-03-14 03:58:24 +0300

    Correct Task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,5 +2,5 @@
 
 SELECT cities.id, cities.name, states.name
     FROM hbtn_0d_usa.cities
-    LEFT OUTER JOIN states ON states.id
+    INNER JOIN states ON cities.state_id = states.id
     ORDER BY cities.id ASC;
