cbd1d62037200a93e0b91e74f8bef12ea706c8ab
cjmatheka <cjmatheka@gmail.com>
2024-03-14 04:04:11 +0300

    Correct Task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- Lists all cities in a database
 
 SELECT cities.id, cities.name, states.name
-    FROM hbtn_0d_usa.cities
-    INNER JOIN hbtn_0d_usa.states
+    FROM cities
+    INNER JOIN states
         ON cities.state_id = states.id
     ORDER BY cities.id ASC;
