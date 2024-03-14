2fe9eff442388bb6bff09609503fd467c1d1c4c9
cjmatheka <cjmatheka@gmail.com>
2024-03-14 03:30:42 +0300

    Add Task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- creates a db and table
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(256) NOT NULL
+);
