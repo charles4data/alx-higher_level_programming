5d4d30e2f3611fbeb81433764ffd195fc2875113
cjmatheka <cjmatheka@gmail.com>
2024-04-08 18:42:40 +0300

    Add Task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const x = process.argv.slice(2);
+
+const size = parseInt(x);
+let i;
+let j;
+
+if (isNaN(size)) {
+  console.log('Missing Size');
+}
+
+for (i = 0; i < size; i++) {
+  let row = '';
+  for (j = 0; j < size; j++) {
+    row +='x';
+  }
+  console.log(row);
+}
