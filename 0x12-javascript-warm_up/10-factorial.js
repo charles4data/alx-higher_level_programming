e7b3e86c936f80249180150576aa3b4e547418bc
cjmatheka <cjmatheka@gmail.com>
2024-04-11 02:08:14 +0300

    Add task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+// computing the factorial
+function factorial (num) {
+  if (isNaN(num)) {
+    return 1;
+  }
+
+  // ensure its a number
+  num = parseInt(num);
+
+  // base case
+  if (num <= 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+
+// getting first argument
+const num = process.argv.slice(2);
+
+// computing the factorial of a given number
+const result = factorial(num);
+
+// printing the result
+console.log(result);
