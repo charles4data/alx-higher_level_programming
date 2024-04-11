a9790bb5ecc6bf6826e0b724043a01e9f724efcd
cjmatheka <cjmatheka@gmail.com>
2024-04-12 00:53:14 +0300

    Update Task 11

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 // importing list
-const list = require('./100-data').list;
+const list1 = require('./100-data').list;
 
 // Using map on the list
-const newList = list.map((x, index) => x * index);
+const newList = list1.map((x, index) => x * index);
 
 // print lists
-console.log(list);
+console.log(list1);
 console.log(newList);
