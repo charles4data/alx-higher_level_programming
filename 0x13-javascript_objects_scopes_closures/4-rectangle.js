2bdeea6875ef879bf377224d5b6f72fa0034c218
cjmatheka <cjmatheka@gmail.com>
2024-04-11 22:59:43 +0300

    Update Task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -18,12 +18,13 @@ module.exports = class Rectangle {
   }
 
   rotate () {
+    const temp = this.width;
     this.width = this.height;
-    this.height = this.width;
+    this.height = temp;
   }
 
   double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
+    this.width *= 2;
+    this.height *= 2;
   }
 };
