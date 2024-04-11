44e4e3a7309ec60f3de887516cd3d2a242d33398
cjmatheka <cjmatheka@gmail.com>
2024-04-11 22:47:15 +0300

    Update Task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/noide
+
+// defining the rectangle class
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if ((w > 0 && Number.isInteger(w)) && (h > 0 && Number.isInteger(h))) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // Create an empty object if w or h is invalid
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
