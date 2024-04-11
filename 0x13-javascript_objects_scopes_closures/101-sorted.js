f6774bc51852e2219ba16193902942433406e262
cjmatheka <cjmatheka@gmail.com>
2024-04-12 00:58:06 +0300

    Add Task 12

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+// 1. Import the dictionary (Assuming 101-data.js is in the same directory)
+const dict = require('./101-data.js').dict;
+
+// 2. Reverse the dictionary
+const occurrencesByUser = {};
+
+for (const userId in dict) {
+  const count = dict[userId];
+  if (!occurrencesByUser[count]) {
+    occurrencesByUser[count] = [];
+  }
+  occurrencesByUser[count].push(userId);
+}
+
+// 3. Print the inverted dictionary
+console.log(occurrencesByUser);
