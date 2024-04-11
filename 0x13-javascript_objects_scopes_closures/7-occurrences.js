dce63d3685049891a7037d1e8b06af0cf3995ac6
cjmatheka <cjmatheka@gmail.com>
2024-04-11 23:41:25 +0300

    Add Task 7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// function definition
+exports.nbOccurences = function (list, searchElement) {
+  let i;
+  let count = 0;
+
+  for (i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
