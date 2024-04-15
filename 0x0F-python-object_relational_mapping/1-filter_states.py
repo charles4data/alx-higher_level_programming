0eca1e4e76912fbbbf5cf7b9c77b8dd7b36daccd
cjmatheka <cjmatheka@gmail.com>
2024-04-15 07:02:55 +0300

    Update Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,20 +5,21 @@
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(
-    host='localhost',
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    database=sys.argv[3],
-    port=3306)
+if __name__ == 'main':
+    db = MySQLdb.connect(
+    	host='localhost',
+    	user=sys.argv[1],
+    	passwd=sys.argv[2],
+    	database=sys.argv[3],
+    	port=3306)
 
-cursor = db.cursor()
-query = 'SELECT * FROM states ORDER BY states.id ASC'
-cursor.execute(query)
-results = cursor.fetchall()
+    cursor = db.cursor()
+    query = 'SELECT * FROM states ORDER BY states.id ASC'
+    cursor.execute(query)
+    results = cursor.fetchall()
 
-for row in results:
-    print(f'({row[0]}, \'{row[1]}\')')
+    for row in results:
+        print(f'({row[0]}, \'{row[1]}\')')
 
-cursor.close()
-db.close()
+    cursor.close()
+    db.close()
