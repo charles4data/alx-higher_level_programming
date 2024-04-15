796201f245109598354d15e77731a8037215013b
cjmatheka <cjmatheka@gmail.com>
2024-04-15 06:58:29 +0300

    Update Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,6 +8,7 @@ from the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
+""" connect to db """
 db = MySQLdb.connect(
     host='localhost',
     user=sys.argv[1],
@@ -15,16 +16,16 @@ db = MySQLdb.connect(
     database=sys.argv[3],
     port=3306)
 
+""" query database """
 cursor = db.cursor()
-
 query = 'SELECT * FROM states ORDER BY states.id ASC'
-
 cursor.execute(query)
 results = cursor.fetchall()
 
-# print in this format(4, 'New York')
+""" display results """
 for row in results:
     print(f'({row[0]}, \'{row[1]}\')')
 
+""" close database """
 cursor.close()
 db.close()
