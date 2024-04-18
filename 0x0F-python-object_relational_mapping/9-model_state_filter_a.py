92adca7b64a10044cd0550a145aefd5ff1907fdd
cjmatheka <cjmatheka@gmail.com>
2024-04-18 20:59:30 +0300

    Add Task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" list ths first state in a database """
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+
+    engine = create_engine(
+        f'mysql://{user}:{passwd}@localhost:3306/{db}')
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    states = session.query(State)
+    .filter(State.name.like('%a%')).order_by(State.id).all()
+
+    for state in states:
+        print(state)
+
+    session.close()
