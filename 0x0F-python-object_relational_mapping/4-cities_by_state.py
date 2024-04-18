cdfdaa705d01c2f41f6a285925442be303ec34d6
cjmatheka <cjmatheka@gmail.com>
2024-04-18 17:56:11 +0300

    Update Task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" lists all cities from database hbtn_0e_4_usa """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306)
+
+    cursor = db.cursor()
+
+    sql_query = """
+    SELECT *
+    FROM cities
+    ORDER BY cities.id ASC
+    """
+
+    results = cursor.execute(sql_query)
+
+    for row in results:
+        print(row)
+
+    cursor.close()
+    db.close()
