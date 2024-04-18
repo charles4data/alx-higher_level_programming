d90c9b756e39f2868c1b422ccfdd728a13b155f2
cjmatheka <cjmatheka@gmail.com>
2024-04-18 20:54:49 +0300

    Add Task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" list all state objects in database """
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+
+    engine = create_engine(
+        f'mysql://{user}:{passwd}@localhost:3306/{db}')
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    first_state = session.query(State).order_by(State.id).first()
+
+    if first_state:
+        print(f"{first_state.id}: {first_state.name}")
+    else:
+        print("Nothing")
+
+    session.close()
