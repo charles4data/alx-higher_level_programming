bdda5879094252d86cb01acb21a68f3804946719
cjmatheka <cjmatheka@gmail.com>
2024-04-25 13:18:19 +0300

    Update task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a POST request with parameters and displays response body
-curl -s "$1" -X POST -H "email=test@gmail.com&subject= I will always be here for PLD"
+curl -s "$1" -X POST -d "email=test@gmail.com&subject= I will always be here for PLD"
