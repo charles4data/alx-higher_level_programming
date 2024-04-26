7e7caab2e986ebaf67ae9d3808ebec52d16640be
Julius Charles <cjmatheka@gmail.com>
2024-04-27 01:19:10 +0300

    Add task 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" The script takes a url and displays x-request-id"""
+
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+    request = urllib.request.Request(url)
+    
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
