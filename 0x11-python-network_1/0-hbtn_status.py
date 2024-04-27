a3d1dac097b9c46f9581f1bfc3b08d759d7753f2
cjmatheka <cjmatheka@gmail.com>
2024-04-28 02:48:58 +0300

    Update Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,14 +3,13 @@
 """The script fetches content from the link"""
 
 
-if __name__ == '__main__':
-    import urllib.request
+import urllib.request
 
-    url = 'https://alx-intranet.hbtn.io/status'
+url = 'https://alx-intranet.hbtn.io/status'
 
-    with urllib.request.urlopen(url) as response:
-        url_content = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(url_content)))
-        print("\t- content: {}".format(url_content))
-        print("\t- utf8 content: {}".format(url_content.decode('utf-8')))
+with urllib.request.urlopen(url) as response:
+    url_content = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(url_content)))
+    print("\t- content: {}".format(url_content))
+    print("\t- utf8 content: {}".format(url_content.decode('utf-8')))
