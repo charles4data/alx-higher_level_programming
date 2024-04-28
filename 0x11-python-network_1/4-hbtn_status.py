29ff7ee88a4c9ca521cab606ca1fbfd9c2a51702
cjmatheka <cjmatheka@gmail.com>
2024-04-28 03:46:10 +0300

    Update Task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""
-A script that fetches https://alx-intranet.hbtn.io/status
+"""A script that:
+ - fetches https://alx-intranet.hbtn.io/status
 """
 
 if __name__ == "__main__":
