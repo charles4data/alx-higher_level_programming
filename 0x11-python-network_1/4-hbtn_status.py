34b89c99ac46d3577f1a491cc936340ee1caa265
cjmatheka <cjmatheka@gmail.com>
2024-04-28 03:41:28 +0300

    Add Task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+A script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+if __name__ == "__main__":
+	import requests
+
+	url = 'https://alx-intranet.hbtn.io/status'
+	html = requests.get(url)
+
+	print("Body Response:")
+	print("\t- type: {}".format(html.text.__class__))
+    print("\t- content: {}".format(html.text))
