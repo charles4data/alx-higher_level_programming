8baae0f66a33ed9b1a5bc21377924bd4cb036c82
cjmatheka <cjmatheka@gmail.com>
2024-04-28 03:11:51 +0300

    Update task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-"""
-This script does the following:
- - Takes url and email as arguments,
- - Sends a POST request to the passed URL, and
- - displays the body of the response (decoded in utf-8)
- """
+"""Takes url and email, and displays response body"""
 
 import urllib.parse
 import urllib.request
@@ -13,6 +8,7 @@ import sys
 
 url = sys.argv[1]
 data = {"email": sys.argv[2]}
+# data = urllib.parse.urlencode(value).encode("ascii")
 
 request = urllib.request.Request(url, data)
 with urllib.request.urlopen(request) as response:
