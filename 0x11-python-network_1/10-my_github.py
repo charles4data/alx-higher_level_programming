cb68649468836a05ad80d99c07c18b391e48aa99
cjmatheka <cjmatheka@gmail.com>
2024-04-28 04:18:00 +0300

    Update Task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,12 +6,12 @@
 
 if __name__ == '__main__':
     import requests
+    from requests.auth import HTTPBasicAuth
     import sys
 
     url = f'https://api.github.com/user'
-    username = sys.argv[1]
-    token = sys.ergv[2]
+    auth = HTTPBasicAuth(sys.argv[1], sys.ergv[2])
 
-    response = requests.get(url, auth=(username, token))
+    response = requests.get(url, auth=auth)
     user_data = response.json()
     print(user_data.get('id'))
