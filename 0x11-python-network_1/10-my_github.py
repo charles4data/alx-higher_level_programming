4f1b98e2baf353aa18bd45559b3afa168e383dc6
cjmatheka <cjmatheka@gmail.com>
2024-04-28 04:14:14 +0300

    Add Task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""The script does the following:
+ - Takes github credentials
+ - Displays github id
+"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    url = 'https://api.github.com/user'
+    username = sys.argv[1]
+    token = sys.ergv[2]
+
+    response = requests.get(url, auth=(username, token))
+    user_data = response.json()
+    print(user_data['id'])
