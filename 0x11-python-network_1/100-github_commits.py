dc3fe2051d62dd9abc6587a64fb6717b8219ca76
cjmatheka <cjmatheka@gmail.com>
2024-04-28 04:27:58 +0300

    Update Task 11

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,8 +8,10 @@ if __name__ == "__main__":
     import sys
     import requests
 
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[2], sys.argv[1])
+    repo = sys.argv[1]
+    user = sys.argv[2]
+
+    url = f"https://api.github.com/repos/{user}/{repo}/commits"
 
     r = requests.get(url)
     commits = r.json()
