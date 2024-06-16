2a873da07c3b600df024e35b89bcd47277ee52b6
cjmatheka <cjmatheka@gmail.com>
2024-06-17 01:56:54 +0300

    Add Task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+// import request
+const request = require('request');
+
+// Assign URL to variable
+const URL = process.argv[2];
+
+// Get and display the status code
+request(URL, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
