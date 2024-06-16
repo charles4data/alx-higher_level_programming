420f693677d8c5586cb18e282239b8fbf4935eb2
cjmatheka <cjmatheka@gmail.com>
2024-06-17 02:01:05 +0300

    Add Task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+// Import the request module
+const request = require('request');
+
+// Get episode number
+const movieid = process.argv[2];
+const API_Endpoint = 'https://swapi-api.hbtn.io/api/films/';
+
+// Prints the title of starwars movie
+request(API_Endpoint + movieid, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
+  }
+});
