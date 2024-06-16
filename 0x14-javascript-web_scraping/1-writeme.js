bfdb986f779f0fca2214d079652d0b5edde3c12c
cjmatheka <cjmatheka@gmail.com>
2024-06-17 01:53:52 +0300

    Update Task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,22 +1,9 @@
 #!/usr/bin/node
 
-// Import Node.js file system module
 const fs = require('fs');
+const file = process.argv[2];
+const string = process.argv[3];
 
-// Check for arguments
-if (process.argv.length < 4) {
-  console.error("Error: Please provide both file path and string to write as arguments.");
-  process.exit(1);
-}
-
-const filePath = process.argv[2];
-const stringToWrite = process.argv[3];
-
-// Write to file using UTF-8 encoding
-fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
-  if (err) {
-    console.error("Error writing to file:", err); // Print error object on failure
-  } else {
-    console.log(`Successfully wrote "${stringToWrite}" to ${filePath}`);
-  }
+fs.writeFile(file, string, 'utf-8', function (err) {
+  if (err) console.log(err);
 });
