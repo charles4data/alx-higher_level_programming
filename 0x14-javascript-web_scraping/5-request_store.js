e8231702b57b5b64089661a29787778fb840f753
cjmatheka <cjmatheka@gmail.com>
2024-06-17 02:19:04 +0300

    Add Task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+// Imports the rquest and file system Modules
+const request = require('request');
+const fs = require('fs');
+
+// Gets ad stores the contents of a webpage and stores it in a file.
+request(process.argv[2], function (err, response, body) {
+  if (err == null) {
+    fs.writeFileSync(process.argv[3], body);
+  }
+});
