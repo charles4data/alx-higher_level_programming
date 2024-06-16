dda28ed2651cd83a1ea25e543c6d5f978ef80509
cjmatheka <cjmatheka@gmail.com>
2024-06-17 02:16:04 +0300

    Update Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,12 +4,12 @@
 const request = require('request');
 
 if (process.argv.length < 3) {
-  console.error("Error: Please provide the Star Wars API URL as an argument.");
+  console.error('Error: Please provide the Star Wars API URL as an argument.');
   process.exit(1);
 }
 
 // Get the API URL from the command line argument
-const apiUrl = process.argv[2]; 
+const apiUrl = process.argv[2];
 const characterId = 18;
 
 // Fetch film data from the API
@@ -21,16 +21,15 @@ request(apiUrl, (error, response, body) => {
 
   // Parse the JSON response
   try {
-    const films = JSON.parse(body).results; 
+    const films = JSON.parse(body).results;
 
     // Filter for films with Wedge Antilles
     const filmsWithWedge = films.filter(film => {
-      return film.characters.includes(apiUrl + "people/" + characterId);
+      return film.characters.includes(apiUrl + 'people/' + characterId);
     });
 
     // Print the count
-    console.log(filmsWithWedge.length); 
-
+    console.log(filmsWithWedge.length);
   } catch (jsonError) {
     console.error('Error parsing JSON:', jsonError);
   }
