abf079a040fd3edf103e46878275e13fd40f1506
cjmatheka <cjmatheka@gmail.com>
2024-06-18 20:29:28 +0300

    Add Tasks 1-6

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,7 @@
+// script that updates the text color of the <header> element to red (#FF0000)
+
+//selecting the element
+const header = document.querySelector('header');
+
+// Changing text colot to red
+header.style.color = '#ff0000'
